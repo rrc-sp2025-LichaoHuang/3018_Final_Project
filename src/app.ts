e8c2ab9beb9c0dd51d8341b7079e3b14479f5547
Lichao Huang <lhuang38@rrc.ca>
2026-04-12 14:02:15 -0500

    feat: endpoint health check.

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,12 @@ import express, { Express } from "express";
 // Initialize Express application
 const app: Express = express();
 
-// Define a route
-app.get("/", (req, res) => {
-    res.send("Hello, World!");
+app.get("/api/v1/health", (req, res) => {
+  res.json({
+    status: "OK",
+    uptime: process.uptime(),
+    timestamp: new Date().toISOString(),
+  });
 });
 
 export default app;
