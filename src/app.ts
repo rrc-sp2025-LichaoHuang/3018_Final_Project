42c39a513d6171594996e42bd43981aa10a7b184
Lichao Huang <lhuang38@rrc.ca>
2026-04-24 12:41:26 -0500

    feat:CORS

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,15 @@ import adminRoutes from "./api/v1/routes/adminRoutes";
 import setupSwagger from "./api/v1/configs/swagger";
 import uploadRoutes from "./api/v1/routes/uploadRoutes";
 import { getHelmetConfig } from "./api/v1/configs/helmetConfig";
+import cors from "cors";
+import { getCorsOptions } from "./api/v1/configs/corsConfig";
 
 // Initialize Express application
 const app: Express = express();
 
 app.use(getHelmetConfig());
+
+app.use(cors(getCorsOptions()));
 
 app.use(express.json());
 
